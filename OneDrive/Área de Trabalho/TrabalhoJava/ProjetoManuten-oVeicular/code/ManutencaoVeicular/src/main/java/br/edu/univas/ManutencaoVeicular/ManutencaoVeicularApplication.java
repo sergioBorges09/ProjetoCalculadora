2a3cf125d37fc66e7d20b1004f50bd08c5d54804
Matheus Borges <matheusvbborges@hotmail.com>
2024-09-19 12:06:32 -0300

    Instalando as bibliotecas

--- OneDrive/Área de Trabalho/TrabalhoJava/ProjetoManuten-oVeicular/code/ManutencaoVeicular/src/main/java/br/edu/univas/ManutencaoVeicular/ManutencaoVeicularApplication.java
+++ OneDrive/Área de Trabalho/TrabalhoJava/ProjetoManuten-oVeicular/code/ManutencaoVeicular/src/main/java/br/edu/univas/ManutencaoVeicular/ManutencaoVeicularApplication.java
@@ -13,6 +13,6 @@ public class ManutencaoVeicularApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		System.out.println("Hello World");
+		System.out.println("Olá Mundo");
 	}
 }
